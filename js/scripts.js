74161edc76a33ae612c5c2d571b9e70f15463091
StellaCea <stela.ceaicovscaia@hotmail.com>
2022-11-10 11:53:18 +0100

    Created Pokemon objects in PokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert("Hello World");
-let favoriteFood = "Pizza";
-document.write(favoriteFood);
+let pokemonList = [
+    {name: "Charmander", height: 6, types: ["rock", "fire"]},
+    {name: "Rattata", height: 3, types: ["food", "speed"]},
+    {name: "Persian", height: 10, types: ["talk", "haughty"]},
+    {name: "Lickitung", height: 12, types: ["saliva", "tingle"]}
+];
